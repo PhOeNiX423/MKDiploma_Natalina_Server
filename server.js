9216f9fb338ae76f27988cee291a4c7a185a43fa
Натальина Дарья Александровна <natalina.daria@gmail.com>
2025-05-09 14:58:25 +0300

    v3.0
    > Получилось поднять сервер на render.com

--- server.js
+++ server.js
@@ -31,16 +31,17 @@ const ProductSchema = new mongoose.Schema({
 const Product = mongoose.model("Product", ProductSchema, "Products");
 
 // Эндпоинт для получения всех товаров
-app.get("", async (req, res) => {
+app.get("/products", async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
   } catch (error) {
-    res.status(500).json({ message: "Ошибка при получении товаров: ", error });
+    res.status(500).json({ message: "Ошибка при получении товаров", error });
   }
 });
 
 // Запуск сервера
-app.listen(5000, () => {
-  console.log("Сервер работает на http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Сервер работает на порту ${PORT}`);
 });
