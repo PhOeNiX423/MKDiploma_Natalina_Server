91eabd3c6d90f7545699747484fcb0ecd80bbb09
Натальина Дарья Александровна <natalina.daria@gmail.com>
2025-05-09 14:10:43 +0300

    v1.1
    > Изменен файл подключения

--- api/get_products.js
+++ api/get_products.js
@@ -1,14 +1,37 @@
-const mongoose = require("mongoose");
-const Product = require("./models/Product");
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const uri = process.env.MONGODB_URI;
+
+const ProductSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  ingredients: String,
+  f_category: String,
+  s_category: String,
+  t_category: [String],
+  images: [String],
+  value: Number,
+  value_descriptor: String,
+  rating: Number,
 });
+
+let Product;
+
+try {
+  Product = mongoose.model("Product");
+} catch {
+  Product = mongoose.model("Product", ProductSchema, "Products");
+}
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
+      await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
       const products = await Product.find();
       res.status(200).json(products);
     } catch (error) {
